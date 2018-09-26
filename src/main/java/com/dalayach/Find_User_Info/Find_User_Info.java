14f2b3dc6a5e5b579a23aba7600e8291657280d0
dalayach <dalayach@masonlive.gmu.edu>
2018-09-25 23:54:57 -0400

    Initial commit

--- src/main/java/com/dalayach/Find_User_Info/Find_User_Info.java
+++ src/main/java/com/dalayach/Find_User_Info/Find_User_Info.java
@@ -9,38 +9,16 @@ import com.dalayach.File_Handler.File_Handler;
 
 /**  Searches for a username on Google+ API and ascertains the user is who the API returned. If so, then, takes the 
  *   data and stores it into variables for use in other classes
- *   
- *   <br /><br />Last updated - 5/30/2015
- *   <br />Program status: Completed
- *
- *   <br /><br />Contact info - dreadheadeddeveloper@gmail.com
- *
- *   @author David Alayachew
- *   @version 1.1
- *
  */
- 
- //TODO
- //--Remove main method after a main class is created, since testing is no longer necessary
-  
- //Also worth noting that in the one time the readURL was used, maxResults, a variable on the API, was set to
- //5, so this will limit number of results on the items array to only 5. To change, change the URL where it 
- //says maxResults
- 
- //SOURCES
- 
- //https://developers.google.com/apis-explorer/?hl=en_US#p/plus/v1/plus.people.search
+
 
 class Find_User_Info
 {
 
    private File_Handler key_Handler = new File_Handler();
 
-   /** Scanner to take in user responses */
    private Scanner scan;
-   /** API key that allows for access to the Google+ API - key may need to be refreshed on occasion on the site itself */
    private final String APIKey;
-   /** String holds a prewritten phrase, makes reading code easier */
    private final String enterYourUserName     = "Enter your Google+ username.";
    final String incorrectResponse     = "Incorrect response";
    private final String nothingEntered        = "You didn't enter anything in.";
@@ -50,15 +28,10 @@ class Find_User_Info
                                              + "dreadheadeddeveloper@gmail.com as this is a bug.";
    private static final String issueWithURL   = "The URL may have been improperly entered or outdated. Email " 
                                              + "dreadheadeddeveloper@gmail.com as this is a bug";
-   /** String that holds username */
    private String userName = "";
-   /** String that holds user id */
    private String userID; 
-   /** String that holds the responses the user gives */
    private String choice;
-   /** String that holds the name provided by API */
    private String displayName;
-   /** boolean that makes sure an internet connection is active */
    private static boolean properConnection = true;
    
    //it is also important to note that for properConnection, value of the var changes upon check,
@@ -67,19 +40,12 @@ class Find_User_Info
    //be updated upon use of an internet specific function, such as readURL(), moreover in the 
    //catch part of a try catch block when using readURL
    
-   /** boolean that makes sure readURL() was properly executed and didn't run into any exceptions */
    private boolean readURLCorrectlyExecuted;
-   /** boolean that ensures the user entered their name properly */
    private boolean enteredProperly = false;
-   /** boolean that ensures the user is the username provided by the API */
    private boolean correctUser = false;
-   /**  */
    private boolean programIsOutdated = false;
-   /** JSONObject that holds the retrieved data from the Google+ API section plus.people.search */
    private JSONObject plus_ppl_search;
-   /** JSONObject that holds individual keys from plus_ppl_search items key */
    private JSONObject plus_ppl_search_items_JSONObjectContainer;
-   /** JSONArray that holds an array of elements from plus_ppl_search items key */
    private JSONArray plus_ppl_search_items;
 
    Find_User_Info()
@@ -87,13 +53,12 @@ class Find_User_Info
    
       APIKey = key_Handler.fetch_Secret("Google+");
    
-      while((!enteredProperly || !correctUser) && properConnection)//while username has not been entered properly 
-      //and/or the correct user has not been found and as long as the connection is still live
+      while((!enteredProperly || !correctUser) && properConnection)
       {
       
-         ensureUserNameProperlyEntered();//ensure username has been properly entered
+         ensureUserNameProperlyEntered();
          
-         if(enteredProperly && properConnection)//is username has been properly entered and internet connection still active
+         if(enteredProperly && properConnection)
          {
          
             verifyUser();//verify the user is the same as the one found
@@ -102,8 +67,7 @@ class Find_User_Info
       
       }//end while loop
       
-      if(properConnection && !programIsOutdated)//if internet connection was active at last data retrieval from internet
-      //and the program has not yet been outdated
+      if(properConnection && !programIsOutdated)
       {
       
          captureDataOnUser();//pull data on user and store into variables
@@ -112,10 +76,6 @@ class Find_User_Info
             
    }
    
-   /** ensures username has been properly entered by checking if username is empty
-    *
-    *
-    */
    void ensureUserNameProperlyEntered()
    {
    
@@ -123,14 +83,14 @@ class Find_User_Info
    
       scan = new Scanner(System.in);
       
-      System.out.println(enterYourUserName);//requests user to enter username
-      userName = scan.nextLine();//takes in user response and stores it into userName
+      System.out.println(enterYourUserName);
+      userName = scan.nextLine();
                
-      if(userName.isEmpty())//if userName is empty
+      if(userName.isEmpty())
       {
         
-         System.out.println(nothingEntered);//informs user
-         enteredProperly = false;//sets false so that program will stay within while loop until username entered properly
+         System.out.println(nothingEntered);
+         enteredProperly = false;
       
       }
       
@@ -147,10 +107,10 @@ class Find_User_Info
             plus_ppl_search_items = (JSONArray)(plus_ppl_search.get("items"));//retrieves the array called data within 
                                            //plus_ppl_search and stores it into JSONArray plus_ppl_search_items
             
-            if(plus_ppl_search_items.isEmpty())//if JSONArray is empty
+            if(plus_ppl_search_items.isEmpty())
             {
                
-               System.out.println(nameDoesntMatchUp);//inform user
+               System.out.println(nameDoesntMatchUp);
                enteredProperly = false;//sets false so that program will stay within loop until username entered properly
                
             }
@@ -182,32 +142,28 @@ class Find_User_Info
       }
    }
          
-   /**   verifies that the user is the one found
-    *
-    *
-    */
    void verifyUser()
    {
    
-      boolean answeredCorrectly = false;//boolean to ensure the user responds correctly to questions posed later in method
+      boolean answeredCorrectly = false;
       correctUser = true;//boolean to ensure the user is the one found
    
-      while(!answeredCorrectly)//user has not yet answered correctly
+      while(!answeredCorrectly)
       {
       
          System.out.println("Is your username " 
-            + displayName + "? (Y/N)");//asks user if their name matches with the one found
+            + displayName + "? (Y/N)");
          
-         choice = scan.nextLine();//stores user response into choice
+         choice = scan.nextLine();
       
-         if(choice.equals("Y")||choice.equals("y"))//if yes
+         if(choice.equals("Y")||choice.equals("y"))
          {
          
-            answeredCorrectly = true;//sets true so that program will escape while loop for user answering correctly
+            answeredCorrectly = true;
             
          }
          
-         else if(choice.equals("N")||choice.equals("n"))//else if user says no
+         else if(choice.equals("N")||choice.equals("n"))
          {
          
             answeredCorrectly = true;//sets true so that program will escape while loop for user answering correctly
@@ -218,7 +174,7 @@ class Find_User_Info
          else//if user did not enter a proper response
          {
          
-            System.out.println(incorrectResponse);//inform user that they did not enter a proper response
+            System.out.println(incorrectResponse);
             answeredCorrectly = false;//sets false to ensure program stays within while loop
          
          }
@@ -251,14 +207,6 @@ class Find_User_Info
       
    }
    
-   /** will return JSON data from a websource 
-    *  
-    *  @return String that contains JSON data
-    *  
-    *  @param webservice
-    *         url of the webservice that will provide the JSON data
-    *
-    */
    static String readURL(String webservice) throws UnknownHostException
    {
       
